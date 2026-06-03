89096942ed8534c8282440ace35f61ab02ff5c5a
welcomedigitalbox-web <welcome.digitalbox@gmail.com>
2026-06-03 14:24:35 +0400

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,7 @@
 import { initializeApp } from 'firebase/app'
 import { getAuth } from 'firebase/auth'
 import { getFirestore } from 'firebase/firestore'
-
+ 
 const firebaseConfig = {
   apiKey: "AIzaSyBsYJBDE5kDQH2iFE4I1LY0S0xNgO6NT7Q",
   authDomain: "careertest-3a4ad.firebaseapp.com",
@@ -10,7 +10,8 @@ const firebaseConfig = {
   messagingSenderId: "804874192098",
   appId: "1:804874192098:web:fe9105a119c9adcf07cabb"
 }
-
+ 
 const app = initializeApp(firebaseConfig)
 export const auth = getAuth(app)
 export const db = getFirestore(app)
+ 
