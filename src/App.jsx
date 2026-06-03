6a140cc49e064e1dde6e134a4298c71a77858c38
welcomedigitalbox-web <welcome.digitalbox@gmail.com>
2026-06-03 14:26:48 +0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import AdminApp from './admin/AdminApp'
+import CareerTest from './test/CareerTest'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<CareerTest />} />
+        <Route path="/admin/*" element={<AdminApp />} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
